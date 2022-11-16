3fb3e28defd7ab0fbb91691395a3e41f86df79de
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-11-15 22:46:53 -0500

    all done

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,37 +1,39 @@
-class BankAccount():
-    def __init__(self):
-        self.balance = 40000
-        print("Welcome to Chase bank.")
-        
-    
-    def __str__(self):
-        status = (f'balance: {self.balance}')
-        return status
+current_balance = 40000
 
-    def deposit(self, amount):
-        print(self.balance)
-        self.balance += input('How much would you like to deposit? ' + int(amount))
+def bank_visit():
+    prompt = input('What would you like to do? (deposit, withdraw, check_balance)\n')
+    if (prompt == 'deposit'):
+        deposit()
+    elif (prompt == 'withdraw'):
+        withdraw()
+    elif (prompt == 'check_balance'):
+        check_balance()
 
-    def withdraw(self, amount):
-        self.balance -= input('How much would you like to withdraw? ' + int(amount))
-        return amount
+    double_check = input("Anything else you'd like to do? (y/n) ")
+    if (double_check.lower() == 'y'):
+        bank_visit()
+        return
+    else:
+        print("Thank you!")
+        return
 
-my_acct = BankAccount()
-print (my_acct)
-prompt = input('What would you like to do? (deposit, withdraw, check_balance)\n')
-if (prompt == 'deposit'):
-    my_acct.deposit
-elif (prompt == 'withdraw'):
-    my_acct.withdraw
-elif (prompt == 'check_balance'):
-    print(my_acct.balance)
-# double_check = input("Anything else you'd like to do? (y/n) ")
+def check_balance():
+    global current_balance
+    print(f"Your current balance is {current_balance}")
 
+def deposit():
+    amount = input('How much would you like to deposit? \n')
+    global current_balance
+    current_balance += int(amount)
+    print(f"Your new balance is {current_balance}")
 
+def withdraw():
+    amount = input('How much would you like to withdraw? \n')
+    global current_balance
+    current_balance -= int(amount)
+    print(f"Your new balance is {current_balance}")
 
-
-    # if (double_check == 'y'):
-        
-
-print("Have a nice day!")
+print("Welcome to the Bank.")
+bank_visit()
+print("Come again!")
 
