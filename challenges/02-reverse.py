c614b270139639d7858a1dfbe0902b95dfa23507
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-11-15 21:05:43 -0500

    reverse done

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,23 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+# option 1 (worked)
+for element in reversed('reverse_me'):
+    print(element)
+
+# option 2 (didn't work)
+test = ['reverse_me']
+test.reverse()
+print(test)
+
+# option 3 (worked)
+def reverse(d):
+    str = ''
+    for i in d:
+        str = i + str    
+    return str
+
+d = 'reverse_me'
+
+print(reverse(d))
